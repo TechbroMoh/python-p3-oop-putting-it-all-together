865c4316f07b0b4b2926957a04a6d9feee8b755e
felix <felixmonari400@gmail.com>
2024-09-10 04:00:59 +0300

    classes for oop

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._set_page_count(page_count)
     
-        
+    def _set_page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+            self._page_count = None
+        else:
+            self._page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        self._set_page_count(value)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._set_size(size)
+        self.condition = 'New'
+
+    def _set_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+            self._size = None
+        else:
+            self._size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._set_size(value)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")  
+        self.condition = 'New'
